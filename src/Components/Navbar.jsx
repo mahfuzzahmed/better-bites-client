ab2ff8af996e8652aa5434fa3729fc49a20f9484
mahfuzzahmed <m.mahfujahmd@gmail.com>
2024-12-21 21:35:16 +0600

    added the navbar and login condition

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,82 @@
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
+import { useContext } from "react";
 
 const Navbar = () => {
+    // const navigate = useNavigate()
+    const { user, userLogOut } = useContext(AuthContext)
+
+    const links = <>
+        <li className="text-white"><NavLink to="/">Home</NavLink></li>
+        <li className="text-white"><NavLink to="/allMovies">Available Food</NavLink></li>
+        <li className="text-white"><NavLink to="/addMovies">Add Food</NavLink></li>
+        <li className="text-white"><NavLink to="/myFavorite">Manage Foods</NavLink></li>
+        <li className="text-white"><NavLink to="/AppDownload">My Food Request</NavLink></li>
+    </>
     return (
         <div>
-            <h2>navbar</h2>
+            <div className="navbar z-50">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
+                    </div>
+                    <Link to='/'><button className=" text-xl text-[#008080] font-bold">Better<span className="text-[#FFC107]">Bites</span></button></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    {user && user?.email ? (
+                        <div className="flex gap-3 items-center ">
+                            {/* Profile Photo */}
+                            <img
+                                src={user?.photoURL}
+                                alt="User Profile"
+                                className="w-10 h-10 rounded-full border border-[#006B5D] object-cover cursor-pointer"
+                            />
+
+                            {/* logout button */}
+
+                            <button
+                                onClick={userLogOut}
+                                className="btn btn-neutral bg-[#006B5D] rounded-lg">
+                                Log-Out
+                            </button>
+
+                        </div>
+
+                    ) : (
+                        <div className="flex gap-4">
+                            <Link to="/auth/login" className="btn btn-neutral rounded-lg bg-[#006B5D]">
+                                Login
+                            </Link>
+                            <Link to="/auth/register" className="btn btn-neutral rounded-lg bg-[#006B5D]">
+                                Register
+                            </Link>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
